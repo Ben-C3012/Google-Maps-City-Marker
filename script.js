4fdb865d8f603ad8d6c8f96210016b6c49abac34
Ben Cosmin <94783151+Ben-C3012@users.noreply.github.com>
2023-01-22 18:38:25 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,4 @@
 
-// NYC Ifram
-// <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d332355.8168591605!2d-73.96124192704453!3d40.76980219781367!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89c24fa5d33f083b%3A0xc80b8f06e177fe62!2sNew%20York%2C%20NY%2C%20USA!5e0!3m2!1sen!2sil!4v1648975721482!5m2!1sen!2sil" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
-//  Montreal 
-// <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d529742.347663434!2d-73.41313426046398!3d45.38172059426953!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4cc91a541c64b70d%3A0x654e3138211fefef!2sMontreal%2C%20QC%2C%20Canada!5e0!3m2!1sen!2sil!4v1648976040579!5m2!1sen!2sil" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
-// Napoli 
-// <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d568400.0287564474!2d13.94066067429429!3d41.09366957319391!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x133b0a3c328d896b%3A0x309e11f99628150!2sMetropolitan%20City%20of%20Naples%2C%20Italy!5e0!3m2!1sen!2sil!4v1648976301835!5m2!1sen!2sil" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
-
 // Get next Button 
 const nextBtn = document.querySelector('.next')
 const prevBtn = document.querySelector('.prev')
@@ -25,11 +18,7 @@ const napoliSrc = 'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d568400.
 // Location number variable
 let locationNumber = 0
 
-
-
 prevBtn.disabled = true
-
-
 
 // next location function 
 function nextLocation() {
@@ -57,12 +46,7 @@ function nextLocation() {
     if(locationNumber !== 0) {
         prevBtn.disabled = false
     }
-
-   
- 
-    
 }
-
 
 // Previous location function 
 function prevLocation() {
@@ -93,10 +77,6 @@ if(locationNumber === 0) {
     prevBtn.disabled = true
 }
 
-
 }
-
-
-
 
 
